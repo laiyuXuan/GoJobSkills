9d8b1d8f4949c54ea44dec2d684d3e4c7c9956fa
laiyuXuan <yuxuan.lai@ijugo.net>
2017-07-14 16:35:53 +0800

    redis params

--- constant/redis.go
+++ constant/redis.go
@@ -1,4 +1,11 @@
 package constant
 
+import "time"
 
+//config
 var REDIS_SERVER = "127.0.0.1:6379"
+var MAX_IDLE = 3
+var IDLE_TIMEOUT = 240 * time.Second
+
+//key
+var KEY_JIANSHU_ARTICLES_LINKS = "jianshu_article_links"
