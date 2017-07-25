4dd00c9414fdefa2b8495abdc730bf9c438e1c7f
laiyuXuan <yuxuan.lai@ijugo.net>
2017-07-25 09:45:00 +0800

    optimize log config

--- log/log.go
+++ log/log.go
@@ -2,16 +2,14 @@ package log
 
 import (
 	"log"
-	"runtime"
 	"bytes"
 	"os"
 )
 
 func GetLogger() *log.Logger{
-	_, file, _, _ := runtime.Caller(0);
 	var buffer bytes.Buffer
 	buffer.WriteString("[")
-	buffer.WriteString(file)
-	buffer.WriteString("] ->>>")
-	return log.New(os.Stdout, buffer.String(), log.LstdFlags)
+	buffer.WriteString("LOG")
+	buffer.WriteString("] --->> ")
+	return log.New(os.Stdout, buffer.String(), log.LstdFlags | log.Llongfile)
 }
